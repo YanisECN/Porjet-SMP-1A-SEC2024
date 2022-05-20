af967b67f63b4ba2f47dea2c301d7574117ac782
yadon95 <email@example.com>
2022-05-20 10:29:16 +0200

    correction case

--- source/lib/carte.cpp
+++ source/lib/carte.cpp
@@ -5,7 +5,10 @@ Carte::Carte(){
 }
 
 const type_case Carte::GetCase(int coordx, int coordy){
-
+    if(coordx <= CARTE_X && coordy <= CARTE_Y){
+        return cases[coordx][coordy];
+    }
+    return CASE_NULL;
 }
 
 int Carte::SetCase(int coordx, int coordy, type_case new_type){
